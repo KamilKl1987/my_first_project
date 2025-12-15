a2e0dfb37527c8bc645359aac8ddd0dea7a31c5e
Kamil <kamil.klepadlo@protonmail.com>
2025-12-15 20:58:29 +0100

    dodanie nowych funkcji

--- src/main.py
+++ src/main.py
@@ -8,6 +8,21 @@ def show_menu() -> None:
     print("0) Wyjście")
 
 
+def add(a: float, b: float) -> float:
+    return a+b 
+
+def subtract(a: float, b: float) -> float:
+    return a-b   
+
+def multiply(a: float, b: float) -> float:
+    return a*b 
+
+def divide(a: float, b: float) -> float | None:
+    if b == 0:
+        return None
+    return a/b
+
+
 def main () -> None:
     while True:
         show_menu()
@@ -23,6 +38,33 @@ def main () -> None:
 
             result = add(first, second)
             print(f"Wynik dodawania: {result}")
+
+        elif choice == "2":
+            first = float(input("Podaj pierwszą liczbę: "))
+            second = float(input("Podaj drugą liczbę: "))
+
+            result = subtract(first, second)
+            print(f"Wynik odejmowania: {result}") 
+
+        elif choice == "3":
+            first = float(input("Podaj pierwszą liczbe: ")) 
+            second = float(input("Podaj drugą liczbę "))
+
+            result = multiply(first, second)
+            print(f"Wynik mnożenia: {result}")   
+
+        elif choice == "4":
+            first = float(input("Podaj pierwszą liczbę "))
+            second = float(input("Podaj drugą liczbę "))
+
+            result = divide(first, second)
+
+            if result is None:
+                print("Nie można dzielić przez zero!")
+            else:
+                print(f"Wynik dzielenia: {result}")        
+
+
         else:
             print(f"Wybrałeś: {choice} (ta opcja będzie dodana w kolejnym kroku)")    
 
